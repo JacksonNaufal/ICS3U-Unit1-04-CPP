6105827cb92aeaddf0ba59b02daadaf97c8bd6da
Jackson Naufal <Jackson.naufal@mths.ca>
2022-02-24 19:52:55 +0000

    AreaPerimeterCalculator

---
+++ areaPerimeterCalculator.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Jackson Naufal All rights reserved
+// Created By Jackson Naufal
+// Created On Feb 2022
+// This is a "Hello, World!" program with proper style"
+
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "If your rectangle has the dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout <<  std::endl;
+    std::cout << "Area is " << (5 * 3) << "cm."  << std::endl;
+    std::cout << "Perimeter is "<< (2 *(5 +3)) << "cm."<< std::endl;
+    std::cout <<  std::endl;
+    std::cout << "Calculation is complete!" << std::endl;
+}
